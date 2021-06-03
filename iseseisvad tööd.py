fb8a6e8edc130785d50c8bcf2bfc82918a97bb76
Alexandr Klis <77440336+semendereye@users.noreply.github.com>
2021-06-03 15:40:44 +0300

    Add files via upload

--- iseseisvad tööd.py
+++ iseseisvad tööd.py
@@ -2,6 +2,135 @@
 #iseseisvad tööd
 #18.01.21
 import random
+import re
+
+#lisa 8
+    #Vaatab kas email on õige
+regex = '^(\w|\.|\_|\-)+[@](\w|\_|\-|\.)+[.]\w{2,3}$'
+
+def check(email):
+    if(re.search(regex, email)):
+        enimi,pnimi,domeen,com = re.split(".|@", email)
+        
+        print("Tere"+ enimi +", sinu email on "+ domeen +"serveris ja domeeniks on sul"+com)
+ 
+    else:
+        print("Vale email")
+ 
+# Põhi kood
+if __name__ == '__main__':
+ 
+    # emaili lisamine
+    email = input(print("Lisa oma Email: "))
+    
+    check(email)
+
+
+
+
+
+
+
+
+input()
+#lisa 7
+mangija2 = 0
+mangija = 0
+arvuti = 0
+arvuti2 = 0
+
+    #korda kõike selles plokis 10 korda
+for i in range(10):
+
+    # genereerim täringu numbri
+    mangija = random.randint(1, 6)
+    mangija2 = random.randint(1, 6)
+    arvuti = random.randint(1, 6)
+    arvuti2 = random.randint(1, 6)
+
+    # kuvab täringu numbri
+    print("Mängija sai: ", mangija+mangija2)
+    print("Arvuti sai: ", arvuti+arvuti2)
+    
+    mangija = mangija + mangija2
+    arvuti = arvuti + arvuti2
+
+    if mangija > arvuti:
+        print("Mängija võitis")
+        arvuti = mangija + 1 
+    elif arvuti > mangija:
+        print("Arvuti võitis")
+        arvuti = mangija + 1
+    else:
+        print("VIIK")
+
+    input("Vajuta Enterit, et edasi liikuda.") 
+
+print("### Game Over ###")
+print("Mängija skoor:", mangija)
+print("Arvuti skoor:", arvuti)
+
+
+
+
+
+#6 lisa
+rahad = {
+    "EEK":"(Eesti kroon)",
+    "EUR":"(Euro)",    
+}
+#küsib valikud
+print ("Võimalikud valikud:")
+for raha in rahad:
+    print (raha + " " + rahad[raha])
+    
+user_choice1 = input("Millest: ")
+user_choice2 = input("Millsesse: ")
+#küsib palju soovid vahetada
+def lisa():
+    kasutajainput = input("Kui palju soovite vahetada? ")
+    amount = float(kasutajainput)
+    conversion = amount / 15.6466
+    print( str(kasutajainput) + "Krooni on väärt " + str(conversion) + " Eurot.")
+
+
+def euro_to_eek():
+    kasutajainput = input("Kui palju soovite vahetada? ")
+    amount = float(kasutajainput)
+    conversion = amount * 15.6466
+    print( str(kasutajainput) + "Eurot on väärt " + str(conversion) + " Krooni.")
+
+#näitab vahetatud vastust
+if user_choice1 == "EEK" and user_choice2 == "EUR":
+    print ("EEK to EUR.")
+    print(lisa())
+
+elif user_choice1 == "EUR" and user_choice2 == "EEK":
+    print ("Euro to EEK.")
+    print(euro_to_eek())
+    
+
+
+
+#5 lisa
+shopping_list = []
+
+print("Mida me võiks poest osta?")
+print("Kirjuta Tehtud kui list on valmis")
+
+while True:
+    new_item = input("> ")
+    if new_item == 'Tehtud':
+        break
+    shopping_list.append(new_item)
+
+print("Siin on sinu list:")
+
+for item in shopping_list:
+    print(item)
+
+
+
 
 #4 lisa
 a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
